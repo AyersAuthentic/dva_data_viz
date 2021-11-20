54e0e6dbdf701aaf8ffd87a0a32f06b0a44ccd94
Ayers, Kevin Andrew <kayers8@gatech.edu>
2021-11-19 17:10:58 -0800

    first attempt plotly chloropleth'

--- maps/chloro.py
+++ maps/chloro.py
@@ -8,7 +8,22 @@ import plotly.express as px
 import numpy as np
 from dash.dependencies import Input,Output
 import dash_table
+from urllib.request import urlopen
+import json
 
+with urlopen('https://raw.githubusercontent.com/plotly/datasets/master/geojson-counties-fips.json') as response:
+    
+    states = json.load(response)
+
+state_dict = {"type": "FeatureCollection"}
+state_dict["features"] = states
+
+
+df = pd.read_csv("test_cases_per.csv", dtype={"fips": str})
+df = df[['fips', 'new_case_per']]
+
+print(df.head())
+print(type(df['new_case_per'][0]))
 
 app = dash.Dash(external_stylesheets = [ dbc.themes.COSMO],)
 
@@ -23,7 +38,33 @@ navbar = dbc.Navbar(id = 'navbar', children = [
     
 ])
 
-app.layout = html.Div(id = 'parant', children = [navbar])
+
+fig = px.choropleth(df, geojson=states, locations='fips', color='new_case_per',
+                           color_continuous_scale="Viridis",
+                           range_color=(0, 500),
+                           scope="usa",
+                           labels={'new_case_per':'cases per 100 thousand'}
+                          )
+
+fig.update_layout(margin={"r":0,"t":0,"l":0,"b":0})
+fig.show()
+
+
+body = dbc.Container([
+
+    dbc.Row([
+        dcc.Graph(id='covid_graph_1', figure = fig),
+    ])
+
+])
+
+app.layout = html.Div(id = 'parant', children = [navbar, body])
+
+
+# Callbacks
+
+
+
 
 
 
