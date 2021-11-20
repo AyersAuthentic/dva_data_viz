0c5528b0f191dbe5a6b7d95437e1ecac419a3a3c
Ayers, Kevin Andrew <kayers8@gatech.edu>
2021-11-19 18:03:52 -0800

    second attempt test case per 100 chloropleth

--- maps/chloro.py
+++ maps/chloro.py
@@ -19,11 +19,9 @@ state_dict = {"type": "FeatureCollection"}
 state_dict["features"] = states
 
 
-df = pd.read_csv("test_cases_per.csv", dtype={"fips": str})
-df = df[['fips', 'new_case_per']]
+df = pd.read_csv("cases_per_100.csv", dtype={"fips": str})
 
-print(df.head())
-print(type(df['new_case_per'][0]))
+df = df[df['state'] != 'NYC']
 
 app = dash.Dash(external_stylesheets = [ dbc.themes.COSMO],)
 
@@ -39,21 +37,17 @@ navbar = dbc.Navbar(id = 'navbar', children = [
 ])
 
 
-fig = px.choropleth(df, geojson=states, locations='fips', color='new_case_per',
-                           color_continuous_scale="Viridis",
-                           range_color=(0, 500),
-                           scope="usa",
-                           labels={'new_case_per':'cases per 100 thousand'}
-                          )
 
-fig.update_layout(margin={"r":0,"t":0,"l":0,"b":0})
-fig.show()
 
 
 body = dbc.Container([
 
     dbc.Row([
-        dcc.Graph(id='covid_graph_1', figure = fig),
+        dcc.Graph(id='covid_graph_1', figure = {}),
+        dcc.Dropdown(id='covid_graph_1_drop', multi=True, value='01/2020',
+                         options=[{'label':x, 'value':x}
+                                  for x in sorted(df['month_year'].unique())],
+                         )
     ])
 
 ])
@@ -62,6 +56,26 @@ app.layout = html.Div(id = 'parant', children = [navbar, body])
 
 
 # Callbacks
+
+@app.callback(
+    Output('covid_graph_1', 'figure'),
+    Input('covid_graph_1_drop', 'value')
+)
+def update_graph(month_year):
+    dff = df[df['month_year']==month_year]
+
+    fig = px.choropleth(dff, locations='state', color='new_case_per',
+                           color_continuous_scale="ylorbr",
+                           range_color=(df['new_case_per'].min(), dff['new_case_per'].max()),
+                           locationmode="USA-states",
+                           scope="usa",
+                           labels={'new_case_per':'cases per 100 thousand'}
+                          )
+
+    fig.update_layout(margin={"r":0,"t":0,"l":0,"b":0})
+
+    
+    return fig
 
 
 
