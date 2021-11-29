e6c2ff86a2ca378aad5a5246a28a2f2781342dce
Vipul Koti <vkoti7@gatech.edu>
2021-11-29 11:48:46 -0800

    Update app.py
    Prediction app update.

--- PRED_APP/app.py
+++ PRED_APP/app.py
@@ -13,6 +13,8 @@ import json
 import csv
 import plotly.graph_objs as go
 
+from numpy import radians, cos, sin
+import plotly.graph_objects as go
 
 
 
@@ -37,52 +39,111 @@ body  = html.Div([
     dbc.Row([
     html.Div([
         "Month",
-        dcc.Dropdown(id="month", value=1,
-                         options=[{'label':x, 'value':x}
-                                  for x in [i for i in range(10)]]),
+dcc.Slider(
+        id='month', min=0, max=20,step=1,
+        marks={
+        0: '0', 1: '1', 2: '2', 3: '3', 4: '4', 5: '5', 6: '6', 7: '7', 8: '8', 9: '9', 10: '10',
+        11: '11', 12: '12', 13: '13', 14: '14', 15: '15', 16: '16', 17: '17', 18: '18', 19: '19', 20: '20'
+        },
+        value=1,
+    ),
+      #  dcc.Dropdown(id="month", value=1,
+      #                   options=[{'label':x, 'value':x}
+     #                           for x in [i for i in range(10)]]),
     ]),
     html.Div([
         "Birth Year",
-        dcc.Dropdown(id="birth_year", value=2,
-                         options=[{'label':x, 'value':x}
-                                  for x in [i for i in range(10)]]),
+    dcc.Slider(
+        id='birth_year', min=0, max=20,step=1,
+        marks={
+        0: '0', 1: '1', 2: '2', 3: '3', 4: '4', 5: '5', 6: '6', 7: '7', 8: '8', 9: '9', 10: '10',
+        11: '11', 12: '12', 13: '13', 14: '14', 15: '15', 16: '16', 17: '17', 18: '18', 19: '19', 20: '20'
+        },
+        value=2,
+    ),
+        #dcc.Dropdown(id="birth_year", value=2,
+         #                options=[{'label':x, 'value':x}
+          #                        for x in [i for i in range(10)]]),
     ]),
     html.Div([
         "Eductation",
-        dcc.Dropdown(id="eeduc", value=3,
-                         options=[{'label':x, 'value':x}
-                                  for x in [i for i in range(10)]])
+    dcc.Slider(
+        id='eeduc', min=0, max=20,step=1,
+        marks={
+        0: '0', 1: '1', 2: '2', 3: '3', 4: '4', 5: '5', 6: '6', 7: '7', 8: '8', 9: '9', 10: '10',
+        11: '11', 12: '12', 13: '13', 14: '14', 15: '15', 16: '16', 17: '17', 18: '18', 19: '19', 20: '20'
+        },
+        value=3,
+    ),
+        #dcc.Dropdown(id="eeduc", value=3,
+         #                options=[{'label':x, 'value':x}
+          #                        for x in [i for i in range(10)]])
     ]),
     html.Div([
         "Mortgage Last Month",
-        dcc.Dropdown(id="mortlmth", value=4,
-                         options=[{'label':x, 'value':x}
-                                  for x in [i for i in range(10)]]),
+    dcc.Slider(
+        id='mortlmth', min=0, max=10,step=1,
+        marks={
+        0: '0', 1: '1', 2: '2', 3: '3', 4: '4', 5: '5', 6: '6', 7: '7', 8: '8', 9: '9', 10: '10',
+        11: '11', 12: '12', 13: '13', 14: '14', 15: '15', 16: '16', 17: '17', 18: '18', 19: '19', 20: '20'
+        },
+        value=4,
+    ),
+        #dcc.Dropdown(id="mortlmth", value=4,
+         #                options=[{'label':x, 'value':x}
+          #                        for x in [i for i in range(10)]]),
     ]),
     html.Div([
         "Mortgage Confidence",
-        dcc.Dropdown(id="mortconf", value=6,
-                         options=[{'label':x, 'value':x}
-                                  for x in [i for i in range(10)]]),
+    dcc.Slider(
+        id='mortconf', min=0, max=10,step=1,
+        marks={
+        0: '0', 1: '1', 2: '2', 3: '3', 4: '4', 5: '5', 6: '6', 7: '7', 8: '8', 9: '9', 10: '10',
+        11: '11', 12: '12', 13: '13', 14: '14', 15: '15', 16: '16', 17: '17', 18: '18', 19: '19', 20: '20'
+        },
+        value=6,
+    ),
+        #dcc.Dropdown(id="mortconf", value=6,
+         #                options=[{'label':x, 'value':x}
+          #                        for x in [i for i in range(10)]]),
     ]),
     html.Div([
         "Income",
-        dcc.Dropdown(id="income", value=7,
-                         options=[{'label':x, 'value':x}
-                                  for x in [i for i in range(10)]]),
+    dcc.Slider(
+        id='income', min=0, max=10,step=1,
+        marks={
+        0: '0', 1: '1', 2: '2', 3: '3', 4: '4', 5: '5', 6: '6', 7: '7', 8: '8', 9: '9', 10: '10',
+        11: '11', 12: '12', 13: '13', 14: '14', 15: '15', 16: '16', 17: '17', 18: '18', 19: '19', 20: '20'
+        },
+        value=7,
+    ),
+        #dcc.Dropdown(id="income", value=7,
+         #                options=[{'label':x, 'value':x}
+          #                        for x in [i for i in range(10)]]),
     ]),
     html.Div([
         "Type of Work",
-        dcc.Dropdown(id="type_work",  value=2,
-                         options=[{'label':x, 'value':x}
-                                  for x in [i for i in range(10)]]),
+    dcc.Slider(
+        id='type_work', min=0, max=10,step=1,
+        marks={
+        0: '0', 1: '1', 2: '2', 3: '3', 4: '4', 5: '5', 6: '6', 7: '7', 8: '8', 9: '9', 10: '10',
+        11: '11', 12: '12', 13: '13', 14: '14', 15: '15', 16: '16', 17: '17', 18: '18', 19: '19', 20: '20'
+        },
+        value=2,
+    ),
+        #dcc.Dropdown(id="type_work",  value=2,
+         #                options=[{'label':x, 'value':x}
+          #                        for x in [i for i in range(10)]]),
 
     ]),
-    html.Div([dbc.Button('Predict', id='predict_submit', n_clicks=0)], style={"text-align":"center"}),
+    #html.Div([dbc.Button('Predict', id='predict_submit', n_clicks=0, size='lg', style={"background":"#3e1e4f", "color":"#fff", "border-color":"#3e1e4f", "min-width":"240px" })], style={"text-align":"center", "padding":"15px" }),
     ])
     
-    , style={"height": "50%"} ,lg=3),
-    dbc.Col(dcc.Graph(id="bar_chart_pred", figure={}))])
+    #, style={"height": "50%"}
+    , width={'size':3,'offset':1,'order':1}#xs=12, sm=12, md=12, lg=3, xl=3
+        ),
+    dbc.Col(dcc.Graph(id="bar_chart_pred", figure={}), width={'size':6,'offset':1,'order':2} #xs=12, sm=12, md=12, lg=7, xl=7
+         )])
    
 ])
 
@@ -91,18 +152,18 @@ app.layout = html.Div(id = 'parant', children = [navbar, body])
 
 @app.callback(
     Output("bar_chart_pred", "figure"), 
-    [Input("predict_submit", 'n_clicks'),
-    State("month", "value"),
-    State("birth_year", "value"),
-    State("eeduc", "value"),
-    State("mortlmth", "value"),
-    State("mortconf", "value"),
-    State("income", "value"),
-    State("type_work", "value"),
+    [#Input("predict_submit", 'n_clicks'),
+    Input("month", "value"),
+    Input("birth_year", "value"),
+    Input("eeduc", "value"),
+    Input("mortlmth", "value"),
+    Input("mortconf", "value"),
+    Input("income", "value"),
+    Input("type_work", "value"),
     ])
 def update_barchart(month, birth_year, eeduc,
                     mortlmth, mortconf, income,
-                    typework, predict_submit):
+                    typework): #predict_submit
 
     print("Button Clicked!!!!!!!")
     Features = ["eeduc", "mortlmth", "mortconf", "income","typework", "prediction", "CDC"]
@@ -110,16 +171,45 @@ def update_barchart(month, birth_year, eeduc,
     prediction = eeduc + mortlmth + mortconf + income + typework
     print(prediction)
     cdc = 75
-
-    df = pd.DataFrame([["Educ", eeduc, cdc], ["Mortlmth", mortlmth, cdc], ["Mortconf", mortconf, cdc], 
-                        ["Income", income, cdc],["Prediction", prediction, cdc],], columns=["Features", "Level", "CDC"])
+    print("vipul")
+    df = pd.DataFrame([[0,"Educ", 0, cdc],[1,"Educ", eeduc, cdc],[0,"Mortlmth", 0, cdc], [1,"Mortlmth", mortlmth, cdc], [0,"Mortconf", 0, cdc], [1,"Mortconf", mortconf, cdc],
+                        [0,"Income", 0, cdc], [1,"Income", income, cdc],[0,"Prediction", 0, cdc],[1,"Prediction", prediction, cdc],], columns=["Pos","Features", "Level", "CDC"])
     print(df)
-
-    fig = px.bar(df, x = "Features", y="Level", color=df.CDC)
+    """
+    fig = px.bar(df, x = "Features", y="Level"
+            #,animation_group = "Features"
+            #,animation_frame = "Pos"
+                 , color="Level"
+                 ,range_y=[0,25]
+                 )
     fig.update_layout(margin={"r":0,"t":10,"l":0,"b":0})
-    return fig
+    """
+    fig = go.Figure(go.Indicator(
+        mode="gauge+number+delta",
+        value=prediction,
+        domain={'x': [0, 1], 'y': [0, 1]},
+        title={'text': "Depression Meter", 'font': {'size': 24}},
+        delta={'reference': 20, 'increasing': {'color': "RebeccaPurple"}},
+        gauge={
+            'axis': {'range': [None, 40], 'tickwidth': 1, 'tickcolor': "darkblue"},
+            'bar': {'color': "darkblue"},
+            'bgcolor': "white",
+            'borderwidth': 2,
+            'bordercolor': "gray",
+            'steps': [
+                {'range': [0, 20], 'color': 'cyan'},
+                {'range': [20, 25], 'color': 'red'},
+                {'range': [25, 40], 'color': 'royalblue'}],
+            'threshold': {
+                'line': {'color': "red", 'width': 4},
+                'thickness': 0.75,
+                'value': 30}}))
 
+    fig.update_layout(paper_bgcolor="lavender", font={'color': "darkblue", 'family': "Arial"})
+
+    return fig
 
 
 if __name__=='__main__':
     app.run_server(debug=True)
+
