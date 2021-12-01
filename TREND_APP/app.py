367667f6fac74cd847ac7680b835a99b86904b86
Vipul Koti <vkoti7@gatech.edu>
2021-11-30 17:33:12 -0800

    Update app.py
    Updated Layout of visualization

--- TREND_APP/app.py
+++ TREND_APP/app.py
@@ -73,66 +73,88 @@ fig2= px.choropleth(df_5, locations='STATE_C', color=df_5["ANXWORRYDWN_NUM_PRED"
 body = dbc.Container([
 
     dbc.Row([
-
-        dcc.Dropdown(id='trends_dropdown_X', multi=False, value='people_vaccinated_per_hundred',
+        html.H1([
+                "Data Trend Visualization Across USA States",
+            ],style={"padding": "10px, 0, 25px, 0","text-align": "center","background-color": "rgb(233 236 239)","color": "#b8632a"}),
+        dbc.Col([
+            html.H3([
+                "Select the Trend for Visualization",
+            ]),
+            dcc.Dropdown(id='trends_dropdown_X', multi=False, value='people_vaccinated_per_hundred',
                      options=[{'label': x, 'value': x}
                               for x in trends],
+                     style={"width": "80%"},
                      ),
-        dcc.Graph(id='covid_trends_X', figure={})
-
-    ]),
-    dbc.Row([
+            html.Div([ html.Strong("Legend:"),
+                html.Ul([
+                    html.Li(html.Strong("WRKLOSS: Work Loss")),
+                    html.Li(html.Strong("KINDWORK: Kind of Work")),
+                    html.Li(html.Strong("MORTLMTH: Mortgage Last Month")),
+                    html.Li(html.Strong("MORTLCONF: Mortgage Next Month Confidence")),
+                    html.Li(html.Strong("CDCCOUNT: COVID Case Count")),
+                    html.Li(html.Strong("REMPCT: Remote work percentage")),
+                    html.Li(html.Strong("ANXIOUS: Anxiety")),
+                ],style={"padding-top": "10px"})
+            ],style={"font-size": "14px","padding" : "10px 0"}),
+        ],xs={'size':10,'order':1}, sm={'size':10,'order':1}, md={'size':4,'order':1}, lg={'size':4,'order':1} , xl={'size':4,'order':1}),
         dbc.Col([
+            dcc.Graph(id='covid_trends_X', figure={})
+        ],xs={'size':10,'order':1}, sm={'size':10,'order':1}, md={'size':8,'order':2}, lg={'size':8,'order':2} , xl={'size':8,'order':2})
+    ],style={"background-color": "#c5cbd0","padding-bottom": "10px","margin-bottom": "50px"}),
+    dbc.Row([
+        html.H1([
+                "Actual & Predicted Data Trend Visualization of Anxiety Worry and Down Index",
+            ],style={"padding": "25px, 0,25px , 0","text-align": "center","background-color": "rgb(233 236 239)","color": "#b8632a"}),
+        dbc.Col([
+            html.H3(["Actual Trend"],style={"text-align": "center"}),
             dcc.Graph(id='covid_trends_Y',figure=fig1)
             ], #width={'size':6,'offset':1,'order':1}
             xs=12, sm=12, md=12, lg=6, xl=6
         ),
         dbc.Col([
+            html.H3(["Predicted Trend"],style={"text-align": "center"}),
             dcc.Graph(id='covid_trends_Z',figure=fig2)
             ], #width={'size':6,'offset':1,'order':1}
             xs=12, sm=12, md=12, lg=6, xl=6
         )
-    ]),
+    ],style={"background-color": "#c5cbd0","padding-bottom": "10px","margin-bottom": "50px"}),
     dbc.Row([
-
-        dcc.Dropdown(id='trends_dropdown', multi=False, value='people_vaccinated_per_hundred',
-                     options=[{'label': x, 'value': x}
-                              for x in trends],
-                     ),
-        dcc.Dropdown(id='trends_dropdown_2', multi=False, value='10/2020',
-                     options=[{'label': x, 'value': x}
-                              for x in month_year],
-                     ),
-        dcc.Graph(id='covid_trends_1', figure={})
-
-    ]),
+        html.H1([
+                "COVID Case Trend Per USA County",
+            ],style={"padding": "25px, 0,25px , 0","text-align": "center","background-color": "rgb(233 236 239)","color": "#b8632a"}),
+        html.Div([
+            html.H3(["Select the Date for Covid Case Trend Visualization"]),
+            html.Span([
+                dcc.DatePickerSingle(
+                    id='my-date-picker-single',
+                    min_date_allowed=date(2020, 1, 21),
+                    max_date_allowed=date(2021, 9, 29),
+                    initial_visible_month=date(2020, 1, 21),
+                    date=date(2020, 1, 21)
+                ),
+            ])
+        ]),
+        dcc.Graph(id='covid_graph_1', figure = {})
+    ],style={"background-color": "#c5cbd0","padding-bottom": "10px","margin-bottom": "50px"}),
 
     dbc.Row([
-        dbc.Col([
-dcc.DatePickerSingle(
-            id='my-date-picker-single',
-            min_date_allowed=date(2020, 1, 21),
-            max_date_allowed=date(2021, 9, 29),
-            initial_visible_month=date(2020, 1, 21),
-            date=date(2020, 1, 21)
-        ),
-            dcc.Graph(id='covid_graph_1', figure = {})
-        ], #width={'size':6,'offset':1,'order':1}
-            xs=12, sm=12, md=12, lg=6, xl=6
-        ),
-        dbc.Col([
-            dcc.DatePickerSingle(
-            id='my-date-picker-single_2',
-            min_date_allowed=date(2020, 3, 15),
-            max_date_allowed=date(2021, 9, 29),
-            initial_visible_month=date(2020, 3, 15),
-            date=date(2020, 3, 15)
-            ),
-            dcc.Graph(id='covid_lockdowns', figure = {})
-        ], #width={'size':4,'offset':1,'order':2}
-            xs=12, sm=12, md=12, lg=6, xl=6
-        )
-    ])
+        html.H1([
+                "COVID Lockdown Trend USA",
+            ],style={"padding": "25px, 0,25px , 0","text-align": "center","background-color": "rgb(233 236 239)","color": "#b8632a"}),
+        html.Div([
+            html.H3(["Select the Date for Covid Lockdown Trend Visualization"]),
+            html.Span([
+                dcc.DatePickerSingle(
+                    id='my-date-picker-single_2',
+                    min_date_allowed=date(2020, 3, 15),
+                    max_date_allowed=date(2021, 9, 29),
+                    initial_visible_month=date(2020, 3, 15),
+                    date=date(2020, 3, 15)
+                ),
+            ])
+        ]),
+        dcc.Graph(id='covid_lockdowns', figure={})
+    ],style={"background-color": "#c5cbd0","padding-bottom": "10px","margin-bottom": "50px"})
 
 ],fluid=False)
 
